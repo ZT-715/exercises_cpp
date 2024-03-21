ab3ee002a0012477eceaa1eb95ea99c8dbb50e7e
Gabriel S.Zanella <gabes.zanella@gmail.com>
2024-03-21 14:14:01 -0300

    fix initializations, assignments and semicolons

--- exercicio2/exercicio2.cpp
+++ exercicio2/exercicio2.cpp
@@ -22,120 +22,123 @@
 
 // USAR MÚLTIPLOS ARQUIVOS
 // NAO É NECESSÁRIO INICIALIZAÇÃO DA LISTA JÁ COM SÉRIE DE VALORES
-
-#include<stdexcept>
-
+//
+#include <stdexcept>
 
 struct element {
     int value;
     struct element* nx;
     struct element* pr;
-}
+};
 
 class List {
-    element* node;
+    element* root;
     int size;
 
 public:
-    explicit List();
+    List();
     int len();
     int get(int position); // position < size
     bool insert(int value);
     bool isin(int value);
     bool remove(int value);
     ~List();
-}
+};
 
 List::List(){
-    node {nullptr};
-    size {0};
+    root = nullptr;
+    size = 0;
 }
-bool List::insert(int value){
-    element* cursor {node};
-    element* new_node {new element};
-    
-    new_node->value {value};
-    new_node->nx {nullptr};
-    new_node->pr {nullptr};
+
+bool List::insert(int value) {
+    element* cursor = root;
+    element* tmp = new element;
+
+    tmp->value = value;
+    tmp->nx = nullptr;
+    tmp->pr = nullptr;
 
     size++;
 
-    if(!node){
-        node {new_node};
-        return true;
-    }
-    
-    if(node->value > new_node->values){
-        new_node->nx {cursor};
-        node->pr {new_node};
-        node {new_node};
-
+    if (!root) {
+        root = tmp;
         return true;
     }
 
-    while(true){
-        if(!cursor->nx){
-            break;
-        }
-        if(cursor->nx->value >= new_node->value){
-            break;
-        }
-        cursor {cursor->nx};
-    }
-    
-    if(cursor->nx){
-        new_node->nx {cursor->nx};
-        new_node->nx->pr {new_node};
-    }
-    cursor->nx {new_node};
+    if (root->value > tmp->value) {
+        tmp->nx = cursor;
+        root->pr = tmp;
+        root = tmp;
 
-    new_node->pr {cursor};
-  
+        return true;
+    }
+
+    while (true) {
+        if (!cursor->nx) {
+            break;
+        }
+        if (cursor->nx->value >= tmp->value) {
+            break;
+        }
+        cursor = cursor->nx;
+    }
+
+    if (cursor->nx) {
+        tmp->nx = cursor->nx;
+        tmp->nx->pr = tmp;
+    }
+    cursor->nx = tmp;
+
+    tmp->pr = cursor;
+
     return true;
 }
 
-int size(){
+int List::len() {
     return size;
 }
-int get(int position){
-   element* cursor {node};
-   
-   if(postition > size) throw std::out_of_range("List access out of range");
 
-   for(int i {0}; i < position; i++){
-        cursor {cursor->nx};
-   }
+int List::get(int position) {
+    element* cursor = root;
 
-   return cursor->value;
+    if (position >= size) {
+        throw std::out_of_range("List access out of range");
+    }
+
+    for (int i = 0; i < position; i++) {
+        cursor = cursor->nx;
+    }
+
+    return cursor->value;
 }
 
-bool isin(int value){
-    element* cursor {node};
+bool List::isin(int value) {
+    element* cursor = root;
 
-    for(int i {0}; i < size; i++){
-        if(cursor->value == value) return true;
-        cursor {cursor->nx};
+    for (int i = 0; i < size; i++) {
+        if (cursor->value == value) return true;
+        cursor = cursor->nx;
     }
     return false;
 }
 
-bool remove(int value){
-    if(!this->isin(value)) return false;
-   
-    element* tmp {nullptr};
-    element* cursor {node};
+bool List::remove(int value) {
+    if (!isin(value)) return false;
 
-    for(int i {0}; i < size; i++){
-        if(cursor->value == value) break;
-        cursor {cursor->nx};
-    }   
+    element* tmp = nullptr;
+    element* cursor = root;
 
-    tmp {cursor};
+    for (int i = 0; i < size; i++) {
+        if (cursor->value == value) break;
+        cursor = cursor->nx;
+    }
 
-    cursor->pr {tmp->nx};
-    cursor->nx {tmp->pr};
+    tmp = cursor;
 
-    if(node==tmp) node {tmp->nx};
+    cursor->pr = tmp->nx;
+    cursor->nx = tmp->pr;
+
+    if (root == tmp) root = tmp->nx;
 
     size--;
 
@@ -143,6 +146,7 @@ bool remove(int value){
     return true;
 }
 
-List::~List(){
-    while(size > 0) remove(get(0));
+List::~List() {
+    while (size > 0) remove(get(0));
 }
+
