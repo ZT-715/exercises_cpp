22534e0f5e435fbbbdb7dbee353a7acbc0212777
Gabriel S.Zanella <gabes.zanella@gmail.com>
2024-04-09 13:42:10 -0300

    Fix Matrix class getters and setters

--- exercicio4/exercicio1.cpp
+++ exercicio4/exercicio1.cpp
@@ -1,16 +1,52 @@
 /*
- Tarefa D:
-Prepare uma classe que represente uma matriz QUADRADA de valores reais, podendo ir até a dimensão 3x3 (de forma a evitar ter que realizar alocação dinâmica de memória). A classe deve ter um construtor solicitando a dimensão, da matriz, minimamente.
-Além disso, a classe deve apresentar os seguintes métodos:
-1) métodos que permitam setar e recuperar cada uma das posições válidas da matriz, de acordo com a dimensão da sua criação
-2) método que permita realizar a soma de duas matrizes (a matriz do objeto com outra matriz informada)
-3) método que permita realizar a subtração de duas matrizes (a matriz do objeto com outra matriz informada)
-4) método que permita determinar o determinante da matriz
-Uma função main() que permita exemplificar a operação da classe.
-*/
+ *  Tarefa D:
+ *
+ * Prepare uma classe que represente uma matriz QUADRADA de valores reais,
+ * podendo ir até a dimensão 3x3 (de forma a evitar ter que realizar alocação
+ * dinâmica de memória). A classe deve ter um construtor solicitando
+ * a dimensão, da matriz, minimamente.
+ *
+ * Além disso, a classe deve apresentar os seguintes métodos:
+ * 1) métodos que permitam setar e recuperar cada uma das posições válidas da
+ *    matriz, de acordo com a dimensão da sua criação
+ *
+ * 2) método que permita realizar a soma de duas matrizes (a matriz do objeto
+ *    com outra matriz informada)
+ *
+ * 3) método que permita realizar a subtração de duas matrizes (a matriz do
+ *    objeto com outra matriz informada)
+ *
+ * 4) método que permita determinar o determinante da matriz
+ * Uma função main() que permita exemplificar a operação da classe.
+ */
+
+/* 
+ * CORREÇÕES NECESSÁRIAS:
+ *
+ *  Definir setter
+ *
+ *  Utilizar os atributos do objeto diretamente em seus métodos, assim
+ *  não cria overhead desnecessário na stack
+ *
+ *  Inicializar matriz zerada
+ *
+ *  ?Remover exeções
+ *
+ *  Ajustar print para ser string
+ *
+ * MELHORIAS NECESSÁRIASS:
+ * 
+ *  Copy constructor
+ *  
+ *  Overload attribuição
+ *
+ */
 
 #include<iostream>
 #include<stdexcept>
+#include<osstream>
+#include<string>
+
 using namespace std;
 
 class Matrix {
@@ -18,12 +54,13 @@ class Matrix {
     int n;
 public:
     Matrix(int i);
-    int size();
-    float get(int, int);
-    int print();
+    int size() const;
+    float get(int, int) const;
+    float set(int, int);
+    cout << string get_matrix() const;
     int sum(Matrix);
     int sub(Matrix);
-    float det();
+    float det() const;
 };
 
 Matrix::Matrix(int i) {
@@ -34,32 +71,57 @@ Matrix::Matrix(int i) {
 
     for (int j = 0; j < n; j++) {
         for (int m = 0; m < n; m++) {
-            matriz[m][j] = m+j+1.0;
+            matriz[m][j] = 0.0;
+        }
+    }
+}
+Matrix::Matrix(const Matrix& m){
+    n = m.size();
+    for (int j = 0; j < n; j++) {
+        for (int m = 0; m < n; m++) {
+            matriz[m][j] = m.get(m,j);
+        }
+    }
+}
+Matrix& Matrix::operator=(const Matrix& m){
+    n = m.size();
+    for (int j = 0; j < n; j++) {
+        for (int m = 0; m < n; m++) {
+            matriz[m][j] = m.get(m,j);
         }
     }
 }
 
-int Matrix::size() {
+int Matrix::size() const{
     return n;
 }
 
-float Matrix::get(int i, int j) {
+float Matrix::get(int i, int j) const{
     if (i < 0 || j < 0 || i >= n || j >= n) {
         throw invalid_argument("Index outside range.");
     }
     return matriz[i][j];
 }
 
-int Matrix::print() {
-    for (int i = 0; i < n; i++) {
-        cout << "[";
-        for (int j = 0; j < n; j++) {
-            cout << get(i, j);
-            if (j >= 0 && j < n-1 ) cout << ", ";
-        }
-        cout << "]" << endl;
+float Matrix::set(int i, int j, float value){
+    if (i < 0 || j < 0 || i >= n || j >= n) {
+        throw invalid_argument("Index outside range.");
     }
-    return 0;
+    return matriz[i][j] = value;
+}
+
+    cout << string Matrix::get_matrix() const{
+    ostringstram oss;
+
+    for (int i = 0; i < n; i++) {
+        oss << "[";
+        for (int j = 0; j < n; j++) {
+            oss << matriz[i][j];
+            if (j >= 0 && j < n-1 ) oss << ", ";
+        }
+        oss << "]" << endl;
+    }
+    return oss.str();
 }
 
 int Matrix::sum(Matrix b) {
@@ -86,11 +148,12 @@ int Matrix::sub(Matrix b) {
     return 0;
 }
 
-float Matrix::det() {
+float Matrix::det() const{
     int pivot;
     float multiplier {0.0};
     float f[3][3];
     float det {1.0};
+    int swaps;
 
     // Convert matrix to float
     for (int i {0}; i < n; i++) {
@@ -98,12 +161,39 @@ float Matrix::det() {
             f[i][j] = matriz[i][j];
         }
     }
+    for (int pivot_index {0}, swaps {0}; swaps < n;){
+        // Diagonal values
+        float pivot = f[pivot_index][pivot_index];
+        int iteration_swaps {0};
+
+        // Se o pivo for zero, carrega a linha até uma
+        // posição que funcione para a fatoração
+        if(pivot == 0.0){
+            int nx_line = (pivot_index + 1)%n;
+            for(int column {0}; column < n; column ++) {
+                float a = matriz[pivot_index][column]; 
+                float b = matriz[nx_line][column];
+
+                matriz[pivot_line][column] = b;
+                matriz[nx_line][column] = a;
+
+                swap++;
+                iteration_swaps++;
+            }
+        }
+        if(pivot_index++ == n){
+            if(iteration_swaps == 0) 
+                break;
+            else
+                pivot_index = 0;
+        }
+
+    }
+
     // Factorize matrix U
     for (int pivot_line {0}; pivot_line < n; pivot_line++) {
         // Diagonal values
         pivot = f[pivot_line][pivot_line];
-
-        if (pivot == 0.0) return 0.0;
 
         // For each line below the current one, subtract the pivot line times the
         // multiplier necessary to zero out the values below the pivot,
@@ -129,11 +219,11 @@ int main(void) {
 
     Matrix a(n);
     cout << endl << "1st matrice: " << endl;
-    a.print();
+    cout << a.get_matrix();
 
     Matrix b(n);
     cout << endl << "2st matrice: " << endl;
-    b.print();
+    cout << b.get_matrix();
  
     cout << endl << "Element at position (0,0) of the 1st matrix: " << a.get(0, 0) << endl;
     cout << endl << "Element at position (0,0) of the 2nd matrix: " << b.get(0, 0) << endl << endl;
@@ -143,21 +233,21 @@ int main(void) {
    
     cout << endl << "Sum of the 1st matrix with the 2nd: " << endl;
     cout << endl << "1st matrice: " << endl;
-    a.print();
+    cout << a.get_matrix();
     cout << endl << "2st matrice: " << endl;
-    b.print();
+    cout << b.get_matrix();
     cout << endl << "Result: " << endl;
     a.sum(b);
-    a.print();
+    cout << a.get_matrix();
 
     cout << endl << "Subtraction of the 1st matrix on the 2nd: " << endl;
     cout << endl << "1st matrice: " << endl;
-    b.print();
+    cout << b.get_matrix();
     cout << endl << "2st matrice: " << endl;
-    a.print();
+    cout << a.get_matrix();
     cout << endl << "Result: " << endl;
     b.sub(a);
-    b.print();
+    cout << b.get_matrix();
 
     return 0;
 }
--- exercicio4/exercicio4.cpp
+++ exercicio4/exercicio4.cpp
@@ -1,11 +1,22 @@
 /*
-Recodifique a sua implantação do exercício 1, criando uma janela adequada com o FLTK, com botões e caixas de texto que permitam realizar o teste pretendido e solicitado. Você provavelmente terá que converter o valor digitado para números (mas estude na FLTK a possibilidade de ter caixas ou usar caixas que permitam apenas a edição de números, por exemplo... existe, por exemplo, o widget Fl_Value_Input).  
-
-Veja sobre esse tema, por exemplo: 
-
-https://www.geeksforgeeks.org/convert-string-to-int-in-cpp/
-
-https://www.geeksforgeeks.org/cpp-string-to-float-double-and-vice-versa/
-
-Além disso, gere as eventuais correções solicitadas nas observações sobre os exercícios (e melhore a implementação das suas classes para um modelo mais próximo das nossas discussões). Minimamente, adicione um copy constructor e sobrescrição da atribuição. No retorno de valores textuais, gere e retorne uma string representativa -- não acione mais diretamente a tela. Você pode gerar um te
+* Recodifique a sua implantação do exercício 1, criando uma janela adequada
+* com o FLTK, com botões e caixas de texto que permitam realizar o teste
+* pretendido e solicitado. Você provavelmente terá que converter o valor
+* digitado para números (mas estude na FLTK a possibilidade de ter caixas ou
+* usar caixas que permitam apenas a edição de números, por exemplo... existe,
+* por exemplo, o widget Fl_Value_Input).
+* 
+* Veja sobre esse tema, por exemplo:
+* 
+* https://www.geeksforgeeks.org/convert-string-to-int-in-cpp/
+* 
+* https://www.geeksforgeeks.org/cpp-string-to-float-double-and-vice-versa/
+* 
+* Além disso, gere as eventuais correções solicitadas nas observações sobre os
+* exercícios (e melhore a implementação das suas classes para um modelo mais
+* próximo das nossas discussões). Minimamente, adicione um copy constructor
+* e sobrescrição da atribuição. No retorno de valores textuais, gere e retorne
+* uma string representativa -- não acione mais diretamente a tela. Você pode
+* gerar um teste intermediário usando o terminal e depois migrar para uma
+* implementação via janelas.
 */
