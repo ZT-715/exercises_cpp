dd09992fcc6e30853d93a59869271e815eece9e4
Gabriel S.Zanella <gabes.zanella@gmail.com>
2024-07-12 12:09:46 -0300

    Implement FILO class
     - must be rewritten to cpp file
     - 3/8 requirements met

--- 5_FILO_class/FILO.hpp
+++ 5_FILO_class/FILO.hpp
@@ -4,9 +4,9 @@
  * inicial desejada (podendo ser zero). A FILO deve permitir
  * a alocação/liberação dinâmica de memória.  A classe deve apresentar métodos
  * que permitam:
- * 1) construtor default, construtor por cópia (deep) e construtor por
+ * V 1) construtor default, construtor por cópia (deep) e construtor por
  *   atribuição (deep)
- * 2) adicionar e remover um elemento da FILO (um método para cada - caso
+ * V 2) adicionar e remover um elemento da FILO (um método para cada - caso
  *   o tamanho anterior da FILO seja ultrapassado, será necessário alocar mais
  *   memória; no caso da remoção, só libere memória e faça a readequação da área
  *   anteriormente alocada caso existam mais de 50% de elementos livres.
@@ -14,11 +14,100 @@
  *   FILO, via operador sobrescrito
  * 4) alterar o valor de um elemento em uma posição válida, arbitrária, da FILO,
  *    via operador sobrescrito
- * 5) retornar a soma de todos os valores atualmente inseridos na FILO.
+ * V 5) retornar a soma de todos os valores atualmente inseridos na FILO.
  * 6) operador sobrescrito que permita a conversão do valor de uma posição da
  *   FILO para uma std::string
- * 7) exceções adequadas que indiquem erros no uso da classe
+ * 7) ex:ceções adequadas que indiquem erros no uso da classe
  * 8) Uma função main() que permita exemplificar a operação da classe. 
 */
 
+#include<stdexcept>
 
+class FILO {
+    private:
+        int size, top;
+        int* head;
+
+    public:
+        FILO(int size): size{size} {
+            if(size < 0) {
+                throw std::invalid_argument("FILO size must not be negative.");
+            } 
+            head = new int[size];         
+        }
+
+        FILO(const FILO& copy): size{copy.size}, top{copy.top} {
+            head = new int[copy.size];
+
+            for(int i; i < top; i++){
+                head[i] = copy.head[i];
+            }
+
+        }
+        FILO operator&=(const FILO& copy) {
+            delete [] head;
+            
+            top = copy.top;
+            size = copy.size;
+            head = new int[copy.size];
+
+            for(int i; i < top; i++){
+                head[i] = copy.head[i];
+            }
+
+        }
+        
+        FILO& add(int a) {
+            if(top + 1 > size){
+                int new_size = size + 1 + size/2;
+                int* new_head = new int[new_size];
+
+                for (int i = 0; i < top; i++)
+                    new_head[i] = head[i];
+
+                delete [] head;
+
+                head = new_head;
+                size = new_size;
+            }
+
+            head[top++] = a;
+            return *this;
+        }
+                
+        int pop() {
+            int a = head[top];
+
+            if (size/2 > top) {
+                int new_size = size/2;
+                int* new_head = new int[new_size];
+
+                for (int i = 0; i < top; i++)
+                    new_head[i] = head[i];
+
+                delete [] head;
+
+                head = new_head;
+                size = new_size;
+            }
+
+            return a;
+        }
+
+//      TODO: 
+//      sobreescrever  [] para atribuição e leitura
+
+        int sum() {
+            int s = 0;
+
+            for(int i = 0; i < top; i++) {
+                s += head[i];
+            }
+            
+            return s;
+        } 
+
+        ~FILO() {
+            delete [] head;
+        }
+};
