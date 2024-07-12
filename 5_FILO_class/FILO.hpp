c507ea147e9bdffee1ab26bfb9b998326cd551a4
Gabriel S.Zanella <gabes.zanella@gmail.com>
2024-07-12 10:45:33 -0300

    Add instructors order

---
+++ 5_FILO_class/FILO.hpp
@@ -0,0 +1,24 @@
+/*
+ * Crie uma classe que represente uma estrutura de uma FILO, de valores tipo
+ * int. A classe deve ter um construtor que indique a quantidade de elementos
+ * inicial desejada (podendo ser zero). A FILO deve permitir
+ * a alocação/liberação dinâmica de memória.  A classe deve apresentar métodos
+ * que permitam:
+ * 1) construtor default, construtor por cópia (deep) e construtor por
+ *   atribuição (deep)
+ * 2) adicionar e remover um elemento da FILO (um método para cada - caso
+ *   o tamanho anterior da FILO seja ultrapassado, será necessário alocar mais
+ *   memória; no caso da remoção, só libere memória e faça a readequação da área
+ *   anteriormente alocada caso existam mais de 50% de elementos livres.
+ * 3) consultar o valor de um elemento em uma posição válida, arbitrária, da
+ *   FILO, via operador sobrescrito
+ * 4) alterar o valor de um elemento em uma posição válida, arbitrária, da FILO,
+ *    via operador sobrescrito
+ * 5) retornar a soma de todos os valores atualmente inseridos na FILO.
+ * 6) operador sobrescrito que permita a conversão do valor de uma posição da
+ *   FILO para uma std::string
+ * 7) exceções adequadas que indiquem erros no uso da classe
+ * 8) Uma função main() que permita exemplificar a operação da classe. 
+*/
+
+
